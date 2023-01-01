3b3ebffa8ca897845771031976aa14eeaba14e62
emresvd <emresevindi99@gmail.com>
2023-01-01 21:59:14 +0300

    modified:   getgithubrepourls/__init__.py

--- getgithubrepourls/__init__.py
+++ getgithubrepourls/__init__.py
@@ -6,13 +6,16 @@ from urllib.parse import urljoin
 class FromBaseURL(object):
     def __init__(self, base_url: str):
         self.base_url = base_url
-        self.urls = []
+
         self.__github_urls = []
         self.__prepare_github_urls()
+
+        self.urls = []
         self.__prepare_urls()
 
     def __prepare_urls(self):
-        pass
+        r = requests.get(self.base_url)
+        soup = BeautifulSoup(r.content, "html.parser")
 
     def __prepare_github_urls(self):
         urls = [
@@ -27,8 +30,6 @@ class FromBaseURL(object):
 
         for i in range(len(urls)):
             urls[i] = "/{}/".format(urls[i])
-            
-        print(urls)
 
-if __name__ == "__main__":
-    repo_urls = FromBaseURL("https://github.com/topics/python")
+        for url in urls:
+            self.__github_urls.append("/{}/".format(url))
