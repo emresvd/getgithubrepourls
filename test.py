484e116dda066f8eff442b42a92d77d811eee0aa
emresvd <emresevindi99@gmail.com>
2023-01-01 23:15:58 +0300

    test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from getgithubrepourls import (
+    FromBaseURL,
+    TopicUrls
+)
+
+topic_urls = TopicUrls()
+
+for url in topic_urls.urls:
+    fromBaseURL = FromBaseURL(url)
+    print(url.split("/")[-1], fromBaseURL.urls)
