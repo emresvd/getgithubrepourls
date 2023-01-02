2f2d09a9933062fc8b3679f72a7d91c9307ffd87
emresvd <emresevindi99@gmail.com>
2023-01-02 17:27:12 +0300

    modified:   README.md	modified:   scripts/write_md.py

--- scripts/write_md.py
+++ scripts/write_md.py
@@ -3,16 +3,22 @@ import os
 with open("main.py", "r", encoding="utf-8") as f:
     main_py = f.read().strip()
 
-output = os.popen("python main.py").read().strip()
+# output = os.popen("python main.py").read().strip()
+
+# data = """
+# ```python
+# {}
+# ```
+# ```python
+# {}
+# ```
+# """.format(main_py, output).strip()
 
 data = """
 ```python
 {}
 ```
-```python
-{}
-```
-""".format(main_py, output).strip()
+""".format(main_py).strip()
 
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(data)
