a598d1b52256e6b437b7c4195d767af5c0711ab1
emresvd <emresevindi99@gmail.com>
2023-01-02 17:20:44 +0300

    modified:   main.py	deleted:    test.py

--- main.py
+++ main.py
@@ -1,38 +1,6 @@
 from getgithubrepourls import (
-    FromBaseURL,
-    TopicUrls,
-    FromExplore,
-    CollectionUrls,
     FromCollections,
     FromTopics
-)
-
-fromBaseURL = FromBaseURL("https://github.com/topics/python")
-print(
-    "fromBaseURL",
-    fromBaseURL.urls,
-    len(fromBaseURL.urls)
-)
-
-topic_urls = TopicUrls()
-print(
-    "topic_urls",
-    topic_urls.urls,
-    len(topic_urls.urls)
-)
-
-fromExplore = FromExplore()
-print(
-    "fromExplore",
-    fromExplore.urls,
-    len(fromExplore.urls)
-)
-
-collectionUrls = CollectionUrls()
-print(
-    "collectionUrls",
-    collectionUrls.urls,
-    len(collectionUrls.urls)
 )
 
 fromCollections = FromCollections()
