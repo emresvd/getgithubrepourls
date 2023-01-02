e39a7dbcbdc67e5ba84a50f63d2fe46b355d0a8f
emresvd <emresevindi99@gmail.com>
2023-01-02 21:17:38 +0300

    modified:   .gitignore	modified:   main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from getgithubrepourls import *
 urls = AllUrlsWithMore()
 
 print(urls.urls)
+print(len(urls.urls))
+
+with open("urls.txt", "w", encoding="utf-8") as f:
+    f.write("\n".join(urls.urls))
