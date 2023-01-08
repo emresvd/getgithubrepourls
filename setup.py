52c1b016897688b9579b842c1dc55db6533c941e
emresvd <emresevindi99@gmail.com>
2023-01-08 13:07:29 +0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='getgithubrepourls',
-    version='1.0.0',
+    version='1.0.1',
     author='emresvd',
     packages=find_packages(),
     install_requires=[
